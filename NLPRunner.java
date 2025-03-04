8ff223a481d40d2bd42ff3350459b9d506d62de8
Cooper Kim <ckim0174@mymail.lausd.net>
2025-03-04 10:38:47 -0800

    Create NLPRunner.java

---
+++ NLPRunner.java
@@ -0,0 +1,28 @@
+public class NLPRunner {
+  public static void main(String[] args) {
+
+/**
+ * Instantiates mynlp; prints out introduction; and prompts user
+ *
+ * Precondition: Must instantiate object
+ * Postcondition: Output inrtoduction
+ *
+ * @param No parameters
+ * @return Overall component
+ */
+
+  MyNLP mynlp = new MyNLP();
+
+  System.out.println("--------------------------------------------------------------");
+  System.out.println("Hello! You will need to create a password for a Login Account");
+  System.out.println(mynlp.promptUser());
+
+
+
+
+   
+   
+   
+   
+  }
+}
